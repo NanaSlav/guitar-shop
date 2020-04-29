c1a2481a475de69b61280b7c3fab7299247da940
NanaSlav <Nastya002@bk.ru>
2020-04-29 16:41:47 +0300

    Home page is created

---
+++ guitarshop/src/test/java/ru/nanaslav/guitarshop/GuitarshopApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nanaslav.guitarshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuitarshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
