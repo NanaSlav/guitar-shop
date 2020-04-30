be737731b50236d76cf8a7e7e14022b4622f569b
NanaSlav <Nastya002@bk.ru>
2020-04-30 19:42:40 +0300

    pagination is added to product list

--- guitarshop/src/main/java/ru/nanaslav/guitarshop/controller/customer/ProductController.java
+++ guitarshop/src/main/java/ru/nanaslav/guitarshop/controller/customer/ProductController.java
@@ -1,15 +1,19 @@
 package ru.nanaslav.guitarshop.controller.customer;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import ru.nanaslav.guitarshop.model.Category;
 import ru.nanaslav.guitarshop.model.Product;
 import ru.nanaslav.guitarshop.repository.ProductRepository;
 import ru.nanaslav.guitarshop.repository.UserRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -20,48 +24,123 @@ public class ProductController {
     @Autowired
     ProductRepository productRepository;
 
+    private void setPages(int page, int last, int size, Model model) {
+        ArrayList<Integer> p = new ArrayList<>();
+        if (page != 1) {
+            p.add(1);
+            if ((page - 3) > 2) {
+                p.add(0);
+                p.add(page - 3);
+                p.add(page - 2);
+                p.add(page - 1);
+                p.add(page);
+            } else {
+                for(int cur = 2; cur <= page; cur++) {
+                    p.add(cur);
+                }
+            }
+        } else {
+            p.add(1);
+        }
+        if (page != last) {
+            if ((page + 1) <= last) {
+                p.add(page + 1);
+                if ((page + 2) <= last) {
+                    p.add(page + 2);
+                    if ((page + 3) <= last) {
+                        p.add(page + 3);
+                        if ((page + 3) < last) {
+                            if ((page + 4) < last ) {
+                                p.add(0);
+                            }
+                            p.add(last);
+                        }
+                    }
+                }
+            }
+        }
+        int prev = (page != 1) ? page - 1 : 1;
+        int next = (page != last) ? page + 1 : last;
+        model.addAttribute("size", size);
+        model.addAttribute("prev", prev);
+        model.addAttribute("next", next);
+        model.addAttribute("pageable", p);
+    }
     @GetMapping("/electric")
-    public String electricGuitars(Model model) {
+    public String electricGuitars(@RequestParam(value = "page", defaultValue = "1") int page,
+                                  @RequestParam(value = "size", defaultValue = "10") int size,
+                                  Model model) {
         String title = "Electric guitars";
         model.addAttribute("title", title);
-        List<Product> products = productRepository.findAllByCategoriesContains(Category.ELECTRIC);
+        PageRequest request = PageRequest.of(page - 1, size);
+        Page<Product> products = productRepository.findAllByCategoriesContains(Category.ELECTRIC, request);
+        int last = products.getTotalPages();
+        setPages(page, last, size, model);
+        model.addAttribute("urlBegin", "/products/electric/");
         model.addAttribute("products", products);
         return "product-list";
     }
 
     @GetMapping("/acoustic")
-    public String acousticGuitars(Model model) {
+    public String acousticGuitars(@RequestParam(value = "page", defaultValue = "1") int page,
+                                  @RequestParam(value = "size", defaultValue = "10") int size,
+                                  Model model) {
         String title = "Acoustic guitars";
         model.addAttribute("title", title);
-        List<Product> products = productRepository.findAllByCategoriesContains(Category.ACOUSTIC);
+        PageRequest request = PageRequest.of(page - 1, size);
+        Page<Product> products = productRepository.findAllByCategoriesContains(Category.ACOUSTIC, request);
+        int last = products.getTotalPages();
+        setPages(page,last,size,model);
+        model.addAttribute("urlBegin", "/products/electric");
         model.addAttribute("products", products);
         return "product-list";
 
     }
 
     @GetMapping("/bass")
-    public String bassGuitars(Model model) {
+    public String bassGuitars(@RequestParam(value = "page", defaultValue = "1") int page,
+                              @RequestParam(value = "size", defaultValue = "10") int size,
+                              Model model) {
         String title = "Bass guitars";
         model.addAttribute("title", title);
-        List<Product> products = productRepository.findAllByCategoriesContains(Category.BASS);
+        model.addAttribute("title", title);
+        PageRequest request = PageRequest.of(page - 1, size);
+        Page<Product> products = productRepository.findAllByCategoriesContains(Category.BASS, request);
+        int last = products.getTotalPages();
+        setPages(page, last, size, model);
+        model.addAttribute("urlBegin", "/products/bass/");
         model.addAttribute("products", products);
         return "product-list";
     }
 
     @GetMapping("/accessories")
-    public String accessories(Model model) {
+    public String accessories(@RequestParam(value = "page", defaultValue = "1") int page,
+                              @RequestParam(value = "size", defaultValue = "10") int size,
+                              Model model) {
         String title = "Accessories";
         model.addAttribute("title", title);
-        List<Product> products = productRepository.findAllByCategoriesContains(Category.ACCESSORY);
+        model.addAttribute("title", title);
+        PageRequest request = PageRequest.of(page - 1, size);
+        Page<Product> products = productRepository.findAllByCategoriesContains(Category.ACCESSORY, request);
+        int last = products.getTotalPages();
+        setPages(page, last, size, model);
+        model.addAttribute("urlBegin", "/products/accessories/");
         model.addAttribute("products", products);
         return "product-list";
     }
 
     @GetMapping("/amplifiers")
-    public String amplifiers(Model model) {
+    public String amplifiers(@RequestParam(value = "page", defaultValue = "1") int page,
+                             @RequestParam(value = "size", defaultValue = "10") int size,
+                             Model model) {
         String title = "Amplifiers";
         model.addAttribute("title", title);
-        List<Product> products = productRepository.findAllByCategoriesContains(Category.AMP);
+        model.addAttribute("title", title);
+        PageRequest request = PageRequest.of(page - 1, size);
+        Page<Product> products = productRepository.findAllByCategoriesContains(Category.AMP, request);
+        int last = products.getTotalPages();
+        setPages(page, last, size, model);
+        model.addAttribute("urlBegin", "/products/amplifiers/");
         model.addAttribute("products", products);
         return "product-list";
     }
--- guitarshop/src/main/java/ru/nanaslav/guitarshop/repository/ProductRepository.java
+++ guitarshop/src/main/java/ru/nanaslav/guitarshop/repository/ProductRepository.java
@@ -1,6 +1,8 @@
 package ru.nanaslav.guitarshop.repository;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import ru.nanaslav.guitarshop.model.Category;
@@ -10,5 +12,5 @@ import java.util.List;
 import java.util.Set;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    List<Product> findAllByCategoriesContains(Category category);
+    Page<Product> findAllByCategoriesContains(Category category, Pageable pageable);
 }
