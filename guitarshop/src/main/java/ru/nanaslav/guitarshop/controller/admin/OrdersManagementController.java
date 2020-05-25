0424d606f9cc1b8b5b1915f5c5ff80fe39447e0e
NanaSlav <Nastya002@bk.ru>
2020-05-25 19:30:01 +0300

    Order details and set order executed for admin

--- guitarshop/src/main/java/ru/nanaslav/guitarshop/controller/admin/OrdersManagementController.java
+++ guitarshop/src/main/java/ru/nanaslav/guitarshop/controller/admin/OrdersManagementController.java
@@ -5,9 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import ru.nanaslav.guitarshop.Pagination;
 import ru.nanaslav.guitarshop.model.Order;
 import ru.nanaslav.guitarshop.repository.OrderRepository;
@@ -34,5 +32,23 @@ public class OrdersManagementController {
         Pagination.setPages(page, orders.getTotalPages(), size, model);
         model.addAttribute("urlBegin", "/admin/orders");
         return "admin/orders";
+    }
+
+    @GetMapping("/{id}")
+    public String orderDetails(@PathVariable(value = "id") long id,
+                               Model model) {
+        Order order = orderRepository.findById(id).orElseThrow(IllegalStateException::new);
+        model.addAttribute("customer", order.getCustomer());
+        model.addAttribute("order", order);
+        model.addAttribute("items", order.getProducts());
+        return "admin/order-details";
+    }
+
+    @PostMapping("/{id}")
+    public String orderExecute(@PathVariable(value = "id") long id) {
+        Order order = orderRepository.findById(id).orElseThrow(IllegalStateException::new);
+        order.setExecuted(true);
+        orderRepository.save(order);
+        return "redirect:/admin/orders";
     }
 }
